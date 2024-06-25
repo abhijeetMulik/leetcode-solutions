eeba6421ad7fe0cff5eff6cecbf7586761205277
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-06-25 14:53:17 -0400

    Time: 54 ms (84.99%), Space: 17.8 MB (31.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mymap = {}
-        for i in range(len(nums)):
-            noToFind = target - nums[i]
-            if nums[i] in mymap:
-                return mymap[nums[i]], i
+        my_map = dict()
+
+        for idx,val in enumerate(nums):
+            noToFind = target - val
+            if noToFind not in my_map:
+                my_map[val] = idx
             else:
-                mymap[noToFind] = i
-        
-        
+                return my_map[noToFind], idx
