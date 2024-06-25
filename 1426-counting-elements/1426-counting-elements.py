982d011523460e4ada19e1cb98d06cc600ce6aba
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-06-25 15:42:05 -0400

    Time: 42 ms (45.38%), Space: 16.6 MB (82.03%) - LeetHub

--- 1426-counting-elements/1426-counting-elements.py
+++ 1426-counting-elements/1426-counting-elements.py
@@ -1,7 +1,8 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         count = 0
+        seen = set(arr)
         for i in arr:
-            if (i + 1) in arr:
+            if (i + 1) in seen:
                 count += 1
         return count
