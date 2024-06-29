d4da6f82c68abca98149d6c32f21162f9088bd26
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-06-29 10:05:08 -0400

    Time: 39 ms (49.91%), Space: 16.5 MB (69.89%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,12 +1,9 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        unique = set()
-        seen = set()
-
-        for i in nums:
-            if i not in unique and i not in seen:
-                unique.add(i)
-            else:
-                unique.discard(i)
-                seen.add(i)
-        return sum(unique)
+        count = Counter(nums)
+        ans = sum(nums)
+        for k, v in count.items():
+            if v > 1:
+                ans -= (k*v)
+        return ans
+        
