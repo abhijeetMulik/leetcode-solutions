22ab270359c4211d76577ea43fbc98c0ccee519c
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-06-29 19:23:19 -0400

    Time: 31 ms (97.25%), Space: 16.7 MB (78.25%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,19 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        myMap = {}
-        if len(s)!= len(t):
+        dic = {}
+        if len(s) != len(t):
             return False
-        for i in range(len(s)):
-            if s[i] not in myMap:
-                if t[i] in myMap.values():
+        for i,n in enumerate(s):
+            if n in dic :
+                if dic[n] != t[i]:
                     return False
-                myMap[s[i]] = t[i]
-            elif myMap.get(s[i]) != t[i]:
-                return False
+            else:
+                if t[i] in dic.values():
+                    return False
+            dic[n] = t[i]
         return True
 
-            
-                
-            
 
         
