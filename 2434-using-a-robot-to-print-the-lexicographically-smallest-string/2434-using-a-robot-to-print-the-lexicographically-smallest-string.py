5ae10e94412a404e2f0c16821fd70b5dbeedac69
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-07-07 19:48:29 -0400

    Time: 508 ms (63.77%), Space: 21 MB (5.58%) - LeetHub

---
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        paper = []
+        t = []
+        min_stack = ['z'] * (len(s) + 1)
+
+        for i in range(len(s) - 1, -1, -1):
+            min_stack[i] = min(min_stack[i + 1], s[i])
+        
+        for i in range(len(s)):
+            t.append(s[i])
+            while t and t[-1] <= min_stack[i + 1]:
+                paper.append(t.pop())
+        
+        return ''.join(paper)
+
+
+
+
+        
