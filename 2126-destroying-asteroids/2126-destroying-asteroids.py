99e8799ebd7562d04a35a61dbf3a431620237147
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-07-30 15:59:51 -0400

    Time: 868 ms (20.94%), Space: 30.3 MB (79.7%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,11 +1,12 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         n = len(asteroids)
-        heapq.heapify(asteroids)
+        asteroids.sort()
+        # heapq.heapify(asteroids)
         for i in range(n):
-            asteroid = heapq.heappop(asteroids)
-            if asteroid <= mass:
-                mass += asteroid
+            # asteroid = heapq.heappop(asteroids)
+            if asteroids[i] <= mass:
+                mass += asteroids[i]
             else:
                 return False
         return True
