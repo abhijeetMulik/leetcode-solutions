db6d6616cdea8861903a26fa6d322b64d7d70059
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-08-06 17:30:25 -0400

    Time: 103 ms (36.15%), Space: 26 MB (5.14%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -6,8 +6,10 @@ class Solution:
             if c not in tmp:
                 tmp.append(c)
             else:
+                # print(tmp)
                 result.append(tmp)
                 tmp = [c]
         result.append(tmp)
+        # print('res: ', result)
         return len(result)
         
