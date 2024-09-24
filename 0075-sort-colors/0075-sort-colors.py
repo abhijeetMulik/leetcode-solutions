47741053362c13ee46e22bf096b141d95fc3d83a
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-09-24 09:48:47 -0400

    Time: 46 ms (6.71%), Space: 16.6 MB (24.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,55 +3,41 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #   QUICKSORT
 
-        i, l, r = 0, 0, len(nums)-1
+        def merge(arr, L, M, R):
+            left = arr[L : M + 1]
+            right = arr[M + 1 : R + 1]
+            i, j, k = L, 0, 0
 
-        def swap(p, q):
-            temp = nums[p]
-            nums[p] = nums[q]
-            nums[q] = temp
+            while j < len(left) and k < len(right):
+                if left[j] < right[k]:
+                    nums[i] = left[j]
+                    j += 1
+                else:
+                    nums[i] = right[k]
+                    k += 1
+                i += 1
+            
+            while j < len(left):
+                nums[i] = left[j]
+                j += 1
+                i += 1
+            
+            while k < len(right):
+                nums[i] = right[k]
+                k += 1
+                i += 1
 
-        while i <= r:
-            if nums[i] == 0:
-                swap(i, l)
-                l +=1
-            elif nums[i] ==2:
-                swap(i, r)
-                r -=1
-                i -=1
-            i +=1
 
+        def divide(arr, l, r):
+            if l == r:
+                return arr
+            m = (l + r) // 2
+            divide(arr, l, m)
+            divide(arr, m + 1, r)
+            merge(arr, l, m, r)
+            return arr
 
-        # MERGE SORT 
-        # def merge(arr, L, M, R):
-        #     left = arr[L:M+1]
-        #     right = arr[M+1:R+1]
-        #     i, j, k = L, 0, 0
+        divide(nums, 0, len(nums))
 
-        #     while j< len(left) and k < len(right):
-        #         if left[j] <= right[k]:
-        #             arr[i] = left[j]
-        #             j +=1
-        #         else:
-        #             arr[i] = right[k]
-        #             k +=1
-        #         i += 1
-        #     while j< len(left):
-        #         arr[i] = left[j]
-        #         j +=1
-        #         i +=1
-        #     while k < len(right):
-        #         arr[i] = right[k]
-        #         k +=1
-        #         i +=1
-
-        # def mergeSort(arr, l, r):
-        #     if l==r:
-        #         return arr
-        #     m = (l+r)//2
-        #     mergeSort(arr, l, m)
-        #     mergeSort(arr, m+1, r)
-        #     merge(arr, l, m, r)
-        #     return arr
-        # return mergeSort(nums, 0, len(nums)-1)
+        
