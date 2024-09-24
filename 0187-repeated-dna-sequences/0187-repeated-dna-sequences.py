23a1f7cfe03b32da5bb8f7fad7eb2918ad0ff2bb
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-09-24 12:18:37 -0400

    Time: 52 ms (83.83%), Space: 27.9 MB (61.81%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,14 +1,15 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        result = set()
-        count = defaultdict(int)
-        for i in range(len(s) - 10):
-            count[s[i:i+10]] += 1
-        
-        for k,v in count.items():
-            if v > 1:
-                result.add(k)
-
-        return list(result)
+        result = []
+        my_map = {}
+        for i in range(len(s)):
+            limit = i + 10
+            if limit <= len(s):
+                substr = s[i:limit]
+                my_map[substr] = 1 + my_map.get(substr, 0)
+        for k, v in my_map.items():
+            if v >= 2:
+                result.append(k)
+        return result
 
         
