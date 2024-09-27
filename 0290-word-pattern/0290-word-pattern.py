ba1bfc206208d9fa44d152629514ff6cff582f9a
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-09-27 13:05:29 -0400

    Time: 36 ms (46.48%), Space: 16.6 MB (39.52%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,15 +1,16 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        myMap = {}
-        s = s.split(' ')
-        if len(pattern) != len(s):
+        s = s.split(' ')  
+        if len(pattern)  != len(s):
             return False
+        dic = defaultdict(str)          
         for i in range(len(pattern)):
-            if pattern[i] not in myMap:
-                if s[i] in myMap.values():
+            if pattern[i] not in dic and s[i] not in dic.values():
+                dic[pattern[i]] = s[i]
+            elif pattern[i] in dic:
+                if dic[pattern[i]] != s[i]:
                     return False
-                myMap[pattern[i]] =s[i]
-            elif myMap[pattern[i]] != s[i]:
+            elif s[i] in dic.values():
                 return False
+
         return True
-        
