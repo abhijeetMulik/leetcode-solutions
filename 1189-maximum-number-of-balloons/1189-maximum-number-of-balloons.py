1b4f5b99b38659bbc608074e9e73047af28e8b6d
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-01 15:02:19 -0400

    Time: 30 ms (93.26%), Space: 16.6 MB (74.14%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         balloon = Counter('balloon')
-        count = Counter(text)
-        result = float('inf')
-        for i in balloon:
-            result = min(result, count[i]//balloon[i])
-        return result
+        string = Counter(text)
+
+        ans = float('inf')
+
+        for c in balloon:
+            ans = min(ans, string[c] // balloon[c])
+
+        return ans
