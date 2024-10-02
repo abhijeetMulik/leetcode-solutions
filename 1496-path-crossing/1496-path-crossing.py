9bc3dbe415e4c204b820fb055c685b646d36e377
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-02 13:44:58 -0400

    Time: 36 ms (49.17%), Space: 16.5 MB (97.84%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.py
+++ 1496-path-crossing/1496-path-crossing.py
@@ -1,24 +1,22 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
         moves = {
-            'N' : (0 , 1),
+            'N' : (0, 1),
             'S' : (0, -1),
             'E' : (1, 0),
             'W' : (-1, 0)
         }
 
         visited = {(0,0)}
-        x, y = 0, 0
-
-        for i in path:
-            dx, dy = moves[i]
-
+        x = y = 0
+        for p in path:
+            dx, dy = moves[p]
             x += dx
             y += dy
 
-            if (x, y) in visited:
+            if (x,y) in visited:
                 return True
             
-            visited.add((x,y))
+            visited.add((x, y))
         return False
         
