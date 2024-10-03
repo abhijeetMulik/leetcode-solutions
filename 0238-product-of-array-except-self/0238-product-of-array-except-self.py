7eb4fc72fcb7262034e8bb121a90d788d7f88858
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-03 13:34:54 -0400

    Time: 2029 ms (5.03%), Space: 26.5 MB (7.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,46 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1 for i in range(len(nums))]
-        prefix = 1
-        for i in range(len(nums)):
-            result[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums)-1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i]
-        return result
+        prefix = [nums[0]]
+        postfix = [nums[-1]]
+        ans = []
 
-
-
-
-
-
-        # prefix = [1 for i in range(len(nums))]
-        # post = [1 for i in range(len(nums))]
-        # result = [1 for i in range(len(nums))]
-
-        # prefix[0] = nums[0]
-
-        # for i in range(1, len(nums)):
-        #     prefix[i] = nums[i]*prefix[i-1]
+        for i in range(1, len(nums)):
+            prefix.append(prefix[-1] * nums[i])
         
-        # post[len(nums)-1] = nums[-1]
+        for i in range(len(nums) - 2, -1, -1):
+            postfix.insert(0, postfix[0] * nums[i])
+        
+        prefix = [1] + prefix + [1]
+        postfix = [1] + postfix + [1]
 
-        # for i in range(len(nums)-2, -1, -1):
-        #     post[i] = post[i+1] * nums[i]
-        
-        # print('pre',prefix)
-        # print('post',post)
-        
-        # post = [1]+post+[1]
-        # prefix = [1] + prefix + [1]
-        
-        # # result[0]=post[0]
-        # result[-1] = prefix[-2]
+        for i in range(1, len(prefix) - 1):
+            ans.append(prefix[i - 1] * postfix[i + 1])
 
-        # for i in range(1,len(nums)+1):
-        #     result[i-1] = (prefix[i-1]*post[i+1])
-        
-        # return result
+        return ans
         
