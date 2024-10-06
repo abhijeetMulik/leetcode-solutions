e850dc0751cfba5cef0a1d6a6dbb6187e5d827ff
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-06 19:51:40 -0400

    Time: 41 ms (91.03%), Space: 17.1 MB (22.48%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,21 +1,22 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        D, R = deque(), deque()
         senate = list(senate)
+        R, D = deque(), deque()
 
-        for i, n in enumerate(senate):
-            if n == "R":
+        for i, c in enumerate(senate):
+            if c == 'R':
                 R.append(i)
             else:
                 D.append(i)
-        
-        while D and R:
+
+        while R and D:
             dTurn = D.popleft()
             rTurn = R.popleft()
 
-            if rTurn < dTurn:
-                R.append(rTurn + len(senate))
-            else:
+            if dTurn < rTurn:
                 D.append(dTurn + len(senate))
+            else:
+                R.append(rTurn + len(senate))
         
-        return "Radiant" if R else "Dire"
+
+        return 'Radiant' if R else 'Dire'
