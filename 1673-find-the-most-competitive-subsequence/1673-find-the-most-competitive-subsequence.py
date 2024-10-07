a63cd1ab1bb41c97999826a3cfff09412cf2d67b
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-06 21:01:19 -0400

    Time: 884 ms (82.74%), Space: 29.8 MB (18.8%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.py
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.py
@@ -1,8 +1,10 @@
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
         stack = []
+        x = len(nums) - k
         for i, n in enumerate(nums):
-            while stack and stack[-1] > n and len(stack) + len(nums) - i > k:
+            while stack and stack[-1] > n and x > 0:
+                x -= 1
                 stack.pop()
             stack.append(n)
         
