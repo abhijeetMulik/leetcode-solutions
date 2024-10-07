8813a12d6be3817bbb2fe318d8f2a5417eaaed7b
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-07 11:14:44 -0400

    Time: 909 ms (60.77%), Space: 36.3 MB (50.89%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,22 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        my_map = {0 : -1}
+        hashmap = {0 : -1}
         total = 0
         for i, n in enumerate(nums):
-            total +=n
+            total += n
             rem = total % k
-            if rem not in my_map:
-                my_map[rem] = i
-            elif i - my_map[rem] > 1:
-                return True
-        return False     
 
-        # for i in range(len(nums)):
-        #     total_sum = nums[i]
-        #     for j in range(i+1, len(nums)):
-        #         total_sum += nums[j]
-        #         if (total_sum%k) == 0:
-        #             return True
-        # return False
+            if rem not in hashmap:
+                hashmap[rem] = i
+            elif i - hashmap[rem] >1:
+                return True
             
+        return False
+
         
