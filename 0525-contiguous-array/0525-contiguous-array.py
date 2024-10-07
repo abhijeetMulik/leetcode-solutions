e9eb86511886ccf6adafed5b230a95a52f857f03
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-07 10:34:49 -0400

    Time: 625 ms (38.52%), Space: 22.2 MB (15.6%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,20 +1,21 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        ans = count = 0
-        dic = defaultdict(int)
+        ans, count = 0, 0
+        hashmap = {}
+
         for i, n in enumerate(nums):
             if n == 0:
                 count += 1
             else:
                 count -= 1
+            
             if count == 0:
                 ans = max(ans, i + 1)
             
-            if count not in dic:
-                dic[count] = i
+            if count not in hashmap:
+                hashmap[count] = i
             else:
-                ans = max(ans, i - dic[count])
-            
-        return ans
+                ans = max(ans, i - hashmap[count])
 
+        return ans
         
