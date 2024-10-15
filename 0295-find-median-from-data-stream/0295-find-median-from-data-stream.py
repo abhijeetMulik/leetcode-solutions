279011ccd2957e56b942724a50dce3869d1dc503
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-14 22:47:09 -0400

    Time: 377 ms (53.08%), Space: 38.2 MB (77.82%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,19 +1,42 @@
 class MedianFinder:
 
     def __init__(self):
-        self.min_heap = []
-        self.max_heap = []
+        self.min_heap = [] # max heap for large
+        self.max_heap = [] # min heap for small
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.max_heap, -num)
-        heapq.heappush(self.min_heap, -(heapq.heappop(self.max_heap)))
-        if len(self.min_heap) > len(self.max_heap):
-            heapq.heappush(self.max_heap, -(heapq.heappop(self.min_heap)))
+        heapq.heappush(self.min_heap, -num)
+        # print('initial min: ', self.min_heap)
+
+        if self.min_heap and self.max_heap and -self.min_heap[0] > self.max_heap[0]:
+            x = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -x)
+        
+        if len(self.min_heap) > len(self.max_heap) + 1:
+            x = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -x)
+        
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            x = heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap, -x)
+        
+        # print('min: ', self.min_heap)
+        # print('max: ',self.max_heap)
+
 
     def findMedian(self) -> float:
+        if len(self.min_heap) > len(self.max_heap):
+            return -self.min_heap[0]
+        
         if len(self.max_heap) > len(self.min_heap):
-            return -(self.max_heap[0])
-        return (self.min_heap[0] - self.max_heap[0]) / 2
+            return self.max_heap[0]
+        
+        
+        return (-self.min_heap[0] + self.max_heap[0]) / 2
+        
+
+
+
         
 
 
