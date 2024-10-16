72e70f1adf07c21154b57db1d9c210a6793688cc
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-15 20:34:10 -0400

    Time: 768 ms (88.56%), Space: 31.3 MB (45.09%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
@@ -1,19 +1,19 @@
 class Solution:
     def halveArray(self, nums: List[int]) -> int:
-        stop = sum(nums)/2
-        nums = [-i for i in nums]
-        heapq.heapify(nums)
-        # print(nums)
-        # print('inital total : ',total)
-        count = 0
-        while stop > 0:
-            first = heapq.heappop(nums)
-            firstHalf = first/2
-            # print('first: ', first)
-            # print('half: ', firstHalf)
-            stop += firstHalf
-            heapq.heappush(nums, firstHalf)
-            count += 1
+        heap = [-n for n in nums]
+        heapq.heapify(heap)
+        target = sum(nums) / 2
+        count, total = 0, 0
+        
 
+        while total < target:
+            x = -heapq.heappop(heap)
+            y = x / 2
+            total += y
+            heapq.heappush(heap, -y)
+            count += 1
+        
         return count
+        
+
         
