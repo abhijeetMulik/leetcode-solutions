91e751f137e3646fcea5bdfc4305028b4dcf96f2
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-28 17:24:14 -0400

    Time: 205 ms (25.23%), Space: 20 MB (31.69%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,31 +1,30 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        q = deque()
-        seen = set()
-        m = len(mat)
-        n = len(mat[0])
-        directions = [(-1,0), (0,-1), (0,1), (1,0)]
 
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n and mat[row][col] == 1
 
+        queue = deque() # row, col, steps
+        directions = [(1,0), (0,1), (-1,0), (0, -1)]
+        seen = set()
+        m = len(mat)
+        n = len(mat[0])
+
         for row in range(m):
             for col in range(n):
                 if mat[row][col] == 0:
-                    q.append((row, col, 1))
+                    queue.append((row, col, 0))
                     seen.add((row, col))
-        
-        while q:
-            row, col, steps = q.popleft()
 
-            for x, y in directions:
-                new_r, new_c = row + y, col + x
-                if (new_r, new_c) not in seen and valid(new_r, new_c):
-                    q.append((new_r, new_c, steps + 1))
-                    seen.add((new_r, new_c))
-                    mat[new_r][new_c] = steps
+        while queue:
+            row, col, steps = queue.popleft()        
+            for dx, dy in directions:
+                new_row, new_col = row + dx, col + dy
+                if valid(new_row, new_col) and (new_row, new_col) not in seen:
+                    seen.add((new_row, new_col))
+                    # print(mat[new_row][new_col], ' : ', steps)
+                    mat[new_row][new_col] = steps + 1
+                    queue.append((new_row, new_col, steps + 1))
 
         return mat
-
-
         
