8a103fd8b1fffa89219f99d8e7ca1ae8137d0300
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-10-31 15:39:19 -0400

    Time: 121 ms (16.58%), Space: 19.3 MB (5.15%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -3,35 +3,34 @@ class Solution:
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n and maze[row][col] == "."
 
+        def invalid(row, col):
+            return row >= m or row < 0 or col >= n or col < 0
+
         m = len(maze)
         n = len(maze[0])
-        q = deque() 
-        q.append([entrance[0], entrance[1], 0]) #row, col, steps
-        # seen = set()
-        # seen.add((entrance[0], entrance[1]))
-        maze[entrance[0]][entrance[1]] = "+"
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        # print('m',m,n)
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+        queue = deque([(entrance[0], entrance[1], 0)]) #row, col, steps
+        seen = {(entrance[0], entrance[1])} #row, col
 
-        while q:
-            row, col, steps = q.popleft()
-            # print('exit condition :', row, ' ', col)
-            
-            for x, y in directions:
-                next_r, next_c = row + y, col + x
-                # print('next row and col: ', next_r, ' ', next_c)
-                # print(valid(next_r, next_c))
-                # print((next_r, next_c) not in seen)
-                if valid(next_r, next_c):
-                    if (next_r in [0, m - 1]  or next_c in [0, n - 1]) : # if this is exit
-                        return steps + 1
-                    # seen.add((next_r, next_c))
-                    maze[next_r][next_c] = "+"
-                    q.append([next_r, next_c, steps + 1])
-                # print('queue: ', q)
-                # print('seen: ', seen)
+        while queue:
+            row, col, steps = queue.popleft()
+            # print(row, col)
+            for dx, dy in directions:
+                new_row, new_col = row + dx, col + dy
+                if valid(new_row, new_col) and (new_row, new_col) not in seen:
+                    seen.add((new_row, new_col))
+                    queue.append((new_row, new_col, steps + 1))
+                elif invalid(new_row, new_col) and steps > 0:
+                    # print(new_row, new_col)
+                    # print(seen)
+                    return steps
+        
 
         return -1
 
 
 
         
+
+
