c2b03260c56ae00fe20381b8d92d57758bc41e7f
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-12 17:20:42 -0500

    Time: 53 ms (11.63%), Space: 17 MB (11.69%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,29 +1,32 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        length = len(board)
-        q = deque([(1, 0)]) # square, moves
+        n = len(board)
+        board.reverse()
+
+        queue = deque([(1, 0)]) #square, move
         seen = set()
 
-        board.reverse()
-        def pos(square):
-            r = (square - 1) // length
-            c = (square - 1) % length
-            if r % 2:
-                c = length - 1 - c       
-            return r, c
+        def calcPosition(square):
+            row = (square - 1) // n
+            col = (square - 1) % n
+            if row % 2:
+                col =  n - 1 - col
+            return row, col
 
-        while q:
-            square, moves = q.popleft()
-
-            for i in range(0, 6):
-                next_sq = square + (i + 1)
-                r, c = pos(next_sq)
-                if board[r][c] != -1:
-                    next_sq = board[r][c]
-                if next_sq == length * length:
+        while queue:
+            square, moves = queue.popleft()
+            for i in range(1, 7):
+                nextSquare = square + i
+                row, col = calcPosition(nextSquare)  
+                print('*************', row, col )
+                if board[row][col] != -1:
+                    nextSquare = board[row][col]
+                if nextSquare == n * n:
                     return moves + 1
-                if next_sq not in seen:
-                    seen.add(next_sq)
-                    q.append([next_sq, moves + 1])
+                if nextSquare not in seen:
+                    seen.add(nextSquare)
+                    queue.append((nextSquare, moves + 1))
+        
         return -1
+
         
