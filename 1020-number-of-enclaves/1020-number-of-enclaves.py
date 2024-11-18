40d9929c71e71176d8f535d62713cd230b803d4f
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-18 12:25:30 -0500

    Time: 173 ms (22.17%), Space: 46.3 MB (16.34%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,33 +1,32 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         seen = set()
-        totalLand, edgeLand = 0, 0
+        edges, islands = 0, 0
         m = len(grid)
         n = len(grid[0])
 
-        def ifEdge(row, col):
+        def isEdge(row, col):
             return row in [0, m - 1] or col in [0, n - 1]
-
-        def isNotValid(row, col):
-            return  row < 0 or row == m or col < 0 or col == n or grid[row][col] == 0
         
-        def dfs(r, c):
-            if isNotValid(r, c) or (r, c) in seen:
+        def isNotValid(row, col):
+            return row < 0 or row >= m or col < 0  or col >= n or grid[row][col] == 0
+        
+        def dfs(row, col):
+            if isNotValid(row,col) or (row,col) in seen:
                 return 0
             ans = 1
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            seen.add((r, c))
-            for x, y in directions:
-                ans += dfs(r + x, c + y)
+            directions = [(0,1), (0,-1), (1,0), (-1,0)]
+            seen.add((row, col))
+            for dx, dy in directions:
+                ans += dfs(row + dx, col + dy)
             return ans
 
-
-        for r in range(m):
-            for c in range(n):
-                totalLand += grid[r][c]
-                if grid[r][c] and (r, c) not in seen and ifEdge(r, c):
-                    edgeLand += dfs(r, c)
-
-        return totalLand - edgeLand
-
+        for row in range(m):
+            for col in range(n):
+                islands += grid[row][col]
+                if grid[row][col] and (row,col) not in seen and isEdge(row,col):
+                    edges += dfs(row,col)
+        # print('islands :', islands)
+        # print('edges: ', edges)
+        return islands - edges
         
