4f877ba5e19381503dbf9c2e7187fb79d7919e9a
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-21 11:32:28 -0500

    Time: 0 ms (100%), Space: 16.8 MB (50.57%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,19 +1,20 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         parent = [i for i in range(26)]
-        def find(x):
-            print(x)
-            if parent[x] != x:
-                return find(parent[x])
-            else:
-                return x
         
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
         for eq in equations:
             if eq[1] == '=':
                 parent[find(ord(eq[3]) - ord('a'))] = find(ord(eq[0]) - ord('a'))
-    
+
         for eq in equations:
             if eq[1] == '!':
                 if find(ord(eq[0]) - ord('a')) == find(ord(eq[3]) - ord('a')):
                     return False
         return True
+        
