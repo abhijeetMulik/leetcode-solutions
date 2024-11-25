a3a0c31877c145a7ffd068111154b28d1ce028a8
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-25 09:42:07 -0500

    Time: 448 ms (5.33%), Space: 44.9 MB (5.13%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,15 +1,17 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         count = Counter(arr)
+        count = dict(sorted(count.items(), key = lambda x : x[1], reverse = True))
         n = len(arr)
-        count = dict(sorted(count.items(), key = lambda x :x[1], reverse = True))
-        val = list(count.values())
-        curr = 0
+        goal = n // 2
+        # arr = set(arr)
         ans = 0
 
-        for left in range(n):
-            curr += val[left]
-            ans += 1
-            if curr >= (n/2):
+        for key in count:
+            if n <= goal :
                 break
+            n -= count[key]
+            ans += 1
+
         return ans
+        
