7e16d4f9699700eb7fdbd4549607e1157af3f15d
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-25 09:15:55 -0500

    Time: 4 ms (60.45%), Space: 16.6 MB (81.59%) - LeetHub

--- 1196-how-many-apples-can-you-put-into-the-basket/1196-how-many-apples-can-you-put-into-the-basket.py
+++ 1196-how-many-apples-can-you-put-into-the-basket/1196-how-many-apples-can-you-put-into-the-basket.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxNumberOfApples(self, weight: List[int]) -> int:
         weight.sort()
-        limit = 5000
         ans = 0
+        curr = 0
         for w in weight:
-            if limit <= 0 or w > limit:
+            curr += w
+            if curr > 5000:
                 break
-            limit -= w
             ans += 1
+            
         return ans
-
 
         
