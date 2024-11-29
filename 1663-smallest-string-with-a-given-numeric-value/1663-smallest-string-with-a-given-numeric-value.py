dbd60b5f43c70a4d4c5a0f7ca647d5be180e8ab7
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-28 21:46:05 -0500

    Time: 303 ms (38.89%), Space: 18.8 MB (29.72%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -1,21 +1,16 @@
 class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
         dic = defaultdict(int)
-        count = 0
-        for a in range(1, 27):
-            dic[a] = chr(count + ord('a'))
-            count += 1
-        ans = ['a'] * n
+        for i in range(26):
+            dic[i + 1] = chr(ord('a') + i)
+        res = ['a'] * n
         for i in range(n, 0, -1):
             limit = k - (i - 1)
             if limit > 26:
-                # ans.append('z')
-                ans[i - 1] = 'z'
+                res[i - 1] = dic[26]
                 k -= 26
             else:
-                # ans.append(dic[limit])
-                ans[i - 1] = dic[limit]
-                k -= limit 
-        return ''.join(ans)
+                res[i - 1] = dic[limit]
+                k -= limit
 
-        
+        return ''.join(res)
