c37e1dc6894e36d75fc0f907e76886e500e4f275
Abhijeet Mulik <93041681+abhijeetMulik@users.noreply.github.com>
2024-11-28 21:06:28 -0500

    Time: 314 ms (37.78%), Space: 17.9 MB (85.94%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -5,15 +5,17 @@ class Solution:
         for a in range(1, 27):
             dic[a] = chr(count + ord('a'))
             count += 1
-        ans = []
+        ans = ['a'] * n
         for i in range(n, 0, -1):
             limit = k - (i - 1)
             if limit > 26:
-                ans.append('z')
+                # ans.append('z')
+                ans[i - 1] = 'z'
                 k -= 26
             else:
-                ans.append(dic[limit])
+                # ans.append(dic[limit])
+                ans[i - 1] = dic[limit]
                 k -= limit 
-        return ''.join(sorted(ans))
+        return ''.join(ans)
 
         
